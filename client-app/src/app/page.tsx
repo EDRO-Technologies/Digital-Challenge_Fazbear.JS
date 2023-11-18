86def0c0aabe3e098ad163f39579599204d6f2bd
Adolf-Eleador-II <fable.chernakov@mail.ru>
2023-11-18 22:16:00 +0500

    form

--- client-app/src/app/page.tsx
+++ client-app/src/app/page.tsx
@@ -1,19 +1,64 @@
 "use client"; // If used in Pages Router, is no need to add "use client"
 
-import React from "react";
-import { App, Button } from "antd";
+import React, { useState } from "react";
+import { App, Button, Modal } from "antd";
+import { Avatar, Skeleton, Switch, Input, Select, InputNumber } from 'antd';
+
+const { TextArea } = Input;
 
 const Home: React.FC = () => {
   const { message } = App.useApp();
 
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setOpen(false);
+  };
+  
   return (
-    <div className="flex flex-row items-center justify-center h-full w-full bg-white rounded-lg">
+    <div className="flex flex-row p-10 justify-center h-full w-full bg-white rounded-lg">
       <div>
-        <Button onClick={() => message.success("Балдеж!")} type="primary">
+        <Button onClick={showModal} type="primary">
           СИНИЙ БОТТОН??? 
         </Button>
+        <Modal title="Заполнение формы"open={open} onOk={handleOk} confirmLoading={confirmLoading} onCancel={handleCancel}>
+          <div  className="flex flex-col gap-3">
+          <div className="flex flex-row gap-3 items-center w-full">
+              <text>Уровень критичности</text>
+              <Select defaultValue=" " className="flex-auto" onChange={() => {}}
+                options={[
+                  { value: '0', label: 'Green' },
+                  { value: '1', label: 'Yellow' },
+                  { value: '2', label: 'Red' },
+                ]}
+              />
+            </div>
+            <div className="flex flex-row gap-3 items-center">
+              <text>Тип события</text>
+              <div className="flex flex-row gap-3"></div>
+            </div>
+            <text>Описание события:</text>
+            <TextArea rows={4}/>
+          </div>
+        </Modal>
       </div>
     </div>
+
+
   );
 };
 
