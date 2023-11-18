262d0d20b2ce4bee5ffed203d50d16651afa4e44
Un1C0re <sayapin.p.a02@gmail.com>
2023-11-19 02:01:09 +0300

    fix new event form

--- client-app/src/app/events/page.tsx
+++ client-app/src/app/events/page.tsx
@@ -1,23 +1,12 @@
 "use client"; // If used in Pages Router, is no need to add "use client"
 
 import React, { useState } from "react";
-import {
-  App,
-  Avatar,
-  Button,
-  Divider,
-  List,
-  Select,
-  Tag,
-  Tooltip,
-  Input,
-  Modal,
-  Timeline,
-} from "antd";
+import { App, Avatar, Button, Divider, List, Select, Tag, Tooltip, Input, Modal } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import EventRecord from "@/components/EventRecord/EventRecord";
-import cl from "./events.module.css";
+import cl from "./events.module.css"
 import BasicLayout from "@/components/BasicLayout";
+import NewEventForm from "@/components/modal/NewEventForm";
 
 const Labeled: React.FC<{ children: any; label: string }> = (props) => {
   return (
@@ -27,6 +16,7 @@ const Labeled: React.FC<{ children: any; label: string }> = (props) => {
     </div>
   );
 };
+
 
 const HistoryList: React.FC = () => {
   const data = [
@@ -65,101 +55,11 @@ const HistoryList: React.FC = () => {
   );
 };
 
-const VersionList: React.FC = () => {
-  const Item: React.FC = () => {
-    return (
-      <div className="flex flex-row justify-between items-start">
-        <div className="flex flex-col">
-          <div className="font-semibold">Тестович О.В</div>
-          <div>Обновил поле ****</div>
-        </div>
-        <Button>Посмотреть</Button>
-      </div>
-    );
-  };
-
-  return (
-    <Timeline
-      className="w-full"
-      mode="left"
-      items={[
-        {
-          label: <div>"2015-09-01"</div>,
-          children: <Item />,
-        },
-        {
-          label: "2015-09-01 09:12:11",
-          children: "Solve initial network problems",
-        },
-        {
-          children: "Technical testing",
-        },
-        {
-          label: "2015-09-01 09:12:11",
-          children: "Network problems being solved",
-        },
-      ]}
-    />
-  );
-};
-
-const ModalAddNewEvent: React.FC<{
-  open: boolean;
-  handleOk: () => void;
-  confirmLoading: boolean;
-  onCancel: () => void;
-}> = ({ open, handleOk, confirmLoading, onCancel }) => {
-  return (
-    <Modal
-      title="Заполнение формы"
-      open={open}
-      onOk={handleOk}
-      confirmLoading={confirmLoading}
-      onCancel={onCancel}
-    >
-      <div className="flex flex-col gap-3">
-        <div className="flex flex-row gap-3 items-center w-full">
-          <text>Название</text>
-          <Input />
-        </div>
-        <div className="flex flex-row gap-3 items-center w-full">
-          <text>Уровень критичности</text>
-
-          <Select
-            defaultValue=" "
-            className="flex-auto"
-            onChange={() => {}}
-            options={[
-              { value: "0", label: "Слабо" },
-              { value: "1", label: "Умеренно" },
-              { value: "2", label: "Сильно" },
-            ]}
-          />
-        </div>
-        <div className="flex flex-row gap-3 items-center">
-          <text>Тип события</text>
-          <Select
-            defaultValue=" "
-            className="flex-auto"
-            onChange={() => {}}
-            options={[
-              { value: "0", label: "начало смены" },
-              { value: "1", label: "закрытие смены" },
-              { value: "2", label: "другое" },
-            ]}
-          />
-        </div>
-        <text>Описание события:</text>
-        <TextArea rows={4} />
-      </div>
-    </Modal>
-  );
-};
-
 const Home: React.FC = () => {
   const { message } = App.useApp();
   const [open, setOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
+
 
   const showModal = () => {
     setOpen(true);
@@ -174,111 +74,109 @@ const Home: React.FC = () => {
   };
 
   const handleCancel = () => {
-    console.log("Clicked cancel button");
+    console.log('Clicked cancel button');
     setOpen(false);
   };
 
   const eventData = [
-    { label: "закрытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Привозка груза", type: 1, date: "12.03.23 8:00" },
-    { label: "Привозка груза", type: 0, date: "12.03.23 8:00" },
-    { label: "Привозка груза", type: 2, date: "12.03.23 8:00" },
-    { label: "Привозка груза", type: 0, date: "12.03.23 8:00" },
-    { label: "Привозка груза", type: 0, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-    { label: "Открытие смены", type: 3, date: "12.03.23 8:00" },
-  ];
+    {label: 'закрытие смены', type: 3, date: "12.03.23 8:00"},
+    {label: 'Привозка груза', type: 1, date: "12.03.23 8:00"},
+    {label: 'Привозка груза', type: 0, date: "12.03.23 8:00"},
+    {label: 'Привозка груза', type: 2, date: "12.03.23 8:00"},
+    {label: 'Привозка груза', type: 0, date: "12.03.23 8:00"},
+    {label: 'Привозка груза', type: 0, date: "12.03.23 8:00"},
+    {label: 'Открытие смены', type: 3, date: "12.03.23 8:00"},
+  ]
 
   return (
     <BasicLayout>
-      <ModalAddNewEvent
-        confirmLoading={confirmLoading}
-        handleOk={handleOk}
-        onCancel={handleCancel}
+    <div className="overflow-auto flex flex-row h-full w-full bg-slate-200 rounded-lg">
+      <Modal
         open={open}
-      />
-      <div className="overflow-auto flex flex-row h-full w-full bg-slate-200 rounded-lg">
-        <div className="flex flex-col max-w-[450px] w-full">
-          <div className="px-4 py-4 bg-slate-100 justify-between flex flex-row">
-            <Button>Экспорт данных</Button>
-            <Button onClick={showModal} type="primary">
-              Добавить
-            </Button>
-          </div>
-
-          <div
-            className={`flex flex-col overflow-y-scroll ${cl.hideScrollBar}`}
-          >
-            {eventData.map((data) => (
-              <EventRecord
-                key={data.date}
-                label={data.label}
-                type={data.type}
-                date={data.date}
-              ></EventRecord>
-            ))}
-          </div>
+        title="Добавить событие"
+        confirmLoading={confirmLoading}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer ={[
+          <Button key="back" onClick={handleCancel} className="border-rose-600 text-rose-600 hover:bg-rose-600">
+            Отменить
+          </Button>,
+          <Button key="submit" onClick={handleCancel} type="primary">
+            Подтвердить
+          </Button>,
+        ]}
+      >
+        <NewEventForm />
+      </Modal>
+      <div className="flex flex-col max-w-[450px] w-full">
+        <div className="px-4 py-4 bg-slate-100 justify-between flex flex-row">
+          <Button>Экспорт данных</Button>
+          <Button onClick={showModal} type="primary">Добавить</Button>
         </div>
 
-        <div className="bg-white w-full h-0 min-h-full px-16 py-4">
-          <div className="flex flex-row justify-between mb-8">
-            <div className="flex flex-row justify-center items-center gap-4">
-              <h1 className="text-2xl">Инцидент #1</h1>
-              <Tag color={"green"}>Обычное</Tag>
-            </div>
-            <Button danger>Редактировать</Button>
-          </div>
-          <div className="flex flex-col gap-4">
-            <Labeled label="Тип:">
-              <Select
-                disabled={true}
-                defaultValue="lucy"
-                style={{ width: 120 }}
-                // onChange={handleChange}
-                options={[
-                  { value: "jack", label: "Команда" },
-                  { value: "lucy", label: "Инцидент" },
-                  { value: "Yiminghe", label: "Критичность" },
-                ]}
-              />
-            </Labeled>
-            <Labeled label="Описание:">
-              <TextArea disabled={true}></TextArea>
-            </Labeled>
-
-            <Labeled label="Кому предназначено:">
-              <Avatar.Group>
-                <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />
-                <a href="https://ant.design">
-                  <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
-                </a>
-                <Tooltip title="Ant User" placement="top">
-                  <Avatar
-                    style={{ backgroundColor: "#87d068" }}
-                    // icon={< />}
-                  />
-                </Tooltip>
-                <Avatar
-                  style={{ backgroundColor: "#1677ff" }}
-                  // icon={<AntDesignOutlined />}
-                />
-              </Avatar.Group>
-            </Labeled>
-          </div>
-          <Divider />
-          <h2 className="text-xl">История изменений</h2>
-          <div className="overflow-y-auto h-64 py-4 px-4 w-auto  flex flex-col justify-start items-start">
-            <VersionList/>
-          </div>
+        <div className={`flex flex-col overflow-y-scroll ${cl.hideScrollBar}`}>
+          {eventData.map((data) =>
+            <EventRecord
+              key={data.date}
+              label={data.label}
+              type={data.type}
+              date={data.date}
+            ></EventRecord>)}
         </div>
       </div>
+
+      <div className="bg-white w-full h-full px-16 py-4">
+        <div className="flex flex-row justify-between mb-8">
+          <div className="flex flex-row justify-center items-center gap-4">
+            <h1 className="text-2xl">Инцидент #1</h1>
+            <Tag color={"green"}>Обычное</Tag>
+          </div>
+          <Button>Редактировать</Button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Labeled label="Тип:">
+            <Select
+              disabled={true}
+              defaultValue="lucy"
+              style={{ width: 120 }}
+              // onChange={handleChange}
+              options={[
+                { value: "jack", label: "Команда" },
+                { value: "lucy", label: "Инцидент" },
+                { value: "Yiminghe", label: "Критичность" },
+              ]}
+            />
+          </Labeled>
+          <Labeled label="Описание:">
+            <TextArea disabled={true}></TextArea>
+          </Labeled>
+
+          <Labeled label="Кому предназначено:">
+            <Avatar.Group>
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />
+              <a href="https://ant.design">
+                <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
+              </a>
+              <Tooltip title="Ant User" placement="top">
+                <Avatar
+                  style={{ backgroundColor: "#87d068" }}
+                  // icon={< />}
+                />
+              </Tooltip>
+              <Avatar
+                style={{ backgroundColor: "#1677ff" }}
+                // icon={<AntDesignOutlined />}
+              />
+            </Avatar.Group>
+          </Labeled>
+        </div>
+        <Divider />
+        <h2 className="text-xl">История изменений</h2>
+        <div className="overflow-auto">
+          <HistoryList />
+        </div>
+      </div>
+    </div>
     </BasicLayout>
   );
 };
