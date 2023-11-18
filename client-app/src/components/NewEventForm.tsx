639320c21715f12156e4836564ca4a7c0ac016f0
Un1C0re <sayapin.p.a02@gmail.com>
2023-11-19 02:47:22 +0300

    add document load to forms

--- client-app/src/components/NewEventForm.tsx
+++ client-app/src/components/NewEventForm.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import type { CascaderProps } from 'antd';
+
 import {
+  Button,
   Form,
-  Input,
-  Select,
+  Input, message,
+  Select, Upload, UploadProps,
 } from 'antd';
+import {UploadOutlined} from "@ant-design/icons";
 
 const { Option } = Select;
 
@@ -16,6 +18,24 @@ const formItemLayout = {
   wrapperCol: {
     xs: { span: 24 },
     sm: { span: 16 },
+  },
+};
+
+const props: UploadProps = {
+  name: 'file',
+  action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
   },
 };
 
@@ -47,51 +67,57 @@ const NewEventForm: React.FC = () => {
   );
 
   return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      style={{ maxWidth: 600 }}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="title"
-        label="Название события"
-        rules={[
-          {
-            required: true,
-            message: 'Введите название события',
-          },
-        ]}
+    <div className="flex flex-col items-center">
+      <Form
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        style={{ maxWidth: 600 }}
+        scrollToFirstError
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="title"
+          label="Название события"
+          rules={[
+            {
+              required: true,
+              message: 'Введите название события',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="event-type"
-        label="Тип события"
-        rules={[{ required: true, message: 'Укажите тип события' }]}
-      >
-        <Input addonAfter={eventTypeSelector} style={{ width: '100%' }} />
-      </Form.Item>
+        <Form.Item
+          name="event-type"
+          label="Тип события"
+          rules={[{ required: true, message: 'Укажите тип события' }]}
+        >
+          <Input addonAfter={eventTypeSelector} style={{ width: '100%' }} />
+        </Form.Item>
 
-      <Form.Item
-        name="event-risk"
-        label="Важность события"
-        rules={[{ required: true, message: 'Please input donation amount!' }]}
-      >
-        <Input addonAfter={eventRiskSelector} style={{ width: '100%' }} />
-      </Form.Item>
+        <Form.Item
+          name="event-risk"
+          label="Важность события"
+          rules={[{ required: true, message: 'Please input donation amount!' }]}
+        >
+          <Input addonAfter={eventRiskSelector} style={{ width: '100%' }} />
+        </Form.Item>
 
-      <Form.Item
-        name="event-description"
-        label="Описание"
-        rules={[{ required: true, message: 'Please input Intro' }]}
-      >
-        <Input.TextArea showCount maxLength={100} />
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name="event-description"
+          label="Описание"
+          rules={[{ required: true, message: 'Please input Intro' }]}
+        >
+          <Input.TextArea showCount maxLength={100} />
+        </Form.Item>
+      </Form>
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Загрузить документ</Button>
+      </Upload>
+    </div>
+
   );
 };
 
